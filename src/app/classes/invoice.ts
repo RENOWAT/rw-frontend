88426f333df4d2246feb1eb440e1df2f49b33507
Jaime <jaimechacons@hotmail.com>
2023-05-28 17:24:39 +0200

    Added invoices area for customer contracts #RNW-21

---
+++ src/app/classes/invoice.ts
@@ -0,0 +1,10 @@
+export class Invoice {
+
+    public id: number;
+    public year: number;
+    public month: number;
+    public amount: number;
+    public consumption: number;
+    public status: string;
+
+}
